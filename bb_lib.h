0702bdd647b0c1f8295c78b7ade85ef9188526fa
Roger Wolff <R.E.Wolff@BitWizard.nl>
2011-02-10 09:14:23 +0000

    forgot bb_lib.h in previous commit

--- bb_lib.h
+++ bb_lib.h
@@ -1,7 +1,6 @@
 #ifndef BB_LIB_H
 #define BB_LIB_H
 
-// test2 rew.
 
 enum bb_types {BB_INVALID = -1,
 	       BB_BYTE, BB_SHORT, BB_INT, BB_FLOAT, BB_BIT};
@@ -9,6 +8,8 @@ enum bb_types {BB_INVALID = -1,
 enum bb_errno {BB_OK = 0,
 	       BB_ENOTFOUND};  // Expand later. 
 
+// Struct bb_var should be opague: It is not defined HERE!. 
+// struct bb_var; 
 
 enum bb_types bb_typestring_to_enum (char *s);
 char * bb_type_enum_to_string (enum bb_types t);
@@ -18,11 +19,17 @@ int bb_typesize (enum bb_types t);
 void bb_init ();
 int bb_get_type (char *name);
 void *bb_get_ptr (char *name);
+struct bb_var *bb_get_handle (char *name);
 void bb_create_var (char *name, int type);
 enum bb_errno bb_overlay_var (char *name1, int type, char *where, int offset);
 int bb_getbitval (void *p);
-void bb_setbit (void *p);
-void bb_clrbit (void *p);
-void bb_writebit (void *p, int val);
+void bb_set_bit (struct bb_var *p);
+void bb_clr_bit (struct bb_var *p);
+void bb_write_bit (struct bb_var *p, int val);
+void bb_write_int (struct bb_var *p, int val);
+void bb_write_float (struct bb_var *p, float val);
+
+int   bb_get_int (struct bb_var *p);
+float bb_get_float (struct bb_var *p);
 
 #endif
