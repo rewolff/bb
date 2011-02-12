02b2df8c68d3842841d2be833a25d6d3cbcb87c7
Roger Wolff <R.E.Wolff@BitWizard.nl>
2011-02-12 14:36:49 +0100

    created bb_mmon.c

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ CFLAGS=-Wall -O2 -ggdb
 #  --std=c99 -pedantic
 
 EXE=bb_create bb_print bb_set bb_mon bb_nom bb_parport bb_ftdi \
-	bb_xor bb_mon_ping bb_list bb_kitt bb_alarm
+	bb_xor bb_mon_ping bb_list bb_kitt bb_alarm bb_mmon
 
 all:$(EXE)
 
@@ -26,6 +26,8 @@ bb_mon_ping: bb_mon_ping.o bb_lib.o
 bb_list: bb_list.o bb_lib.o
 
 bb_alarm: bb_alarm.o bb_lib.o
+
+bb_mmon: bb_mmon.o bb_lib.o
 
 bb_ftdi: bb_ftdi.o
 	$(CC) $< -lftdi -lusb -o $@
---
+++ bb_mmon.c
@@ -0,0 +1,129 @@
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+
+#include "bb_lib.h"
+
+int interval = 100000; // in microseconds. 10x per second.
+
+
+void usage (void)
+{
+  fprintf (stderr, "usage error.\n");
+  exit (1);
+}
+
+
+void mon_int (struct bb_var *p)
+{
+  int new;
+  int old;
+  
+  old = bb_get_int (p) + 1; // Always different!
+  while (1) {
+    new = bb_get_int (p);
+    if (old != new) {
+      printf ("%d\n", new);
+      fflush (stdout); 
+      old = new;
+    }
+    usleep (interval);
+  }
+}
+
+
+void mon_float (struct bb_var *p)
+{
+  float new;
+  float old;
+  
+  old = bb_get_float (p) * 2; // almost always different!
+  while (1) {
+    new = bb_get_float (p);
+    if (old != new) {
+      printf ("%f\n", new);
+      fflush (stdout); 
+      old = new;
+    }
+    usleep (interval);
+  }
+}
+
+
+void mon_varlist (int nvars, int *vartypes, struct bb_var *vars[])
+{
+  int i;
+  union intfloat {
+    int i;
+    float f; 
+  };
+  union intfloat *oldvals;
+
+  oldvals   = malloc (sizeof (union intfloat) * nvars);
+
+  while (1) {
+    for (i=0;i<nvars;i++) {
+      switch (vartypes[i]) {
+      case BB_BYTE: 
+      case BB_SHORT:
+      case BB_INT:  
+      case BB_BIT: 
+	if (oldvals[i].i != bb_get_int (vars[i])) i = nvars + 1;
+	break;
+      case BB_FLOAT:
+	if (oldvals[i].f != bb_get_float (vars[i])) i = nvars + 1;
+	break;
+      }
+    }
+    // if all are the same, we'll come here with i == nvars. 
+    if (i > nvars) {
+      // Some changed! print them all!
+      for (i=0;i<nvars;i++) {
+	switch (vartypes[i]) {
+	case BB_BYTE: 
+	case BB_SHORT:
+	case BB_INT:  
+	case BB_BIT:   
+	  oldvals[i].i =  bb_get_int (vars[i]);
+	  printf ("%d ", oldvals[i].i);
+	  break;
+	case BB_FLOAT: 
+	  oldvals[i].f =  bb_get_float (vars[i]);
+	  printf ("%f ", oldvals[i].f);
+	  break;
+	}
+      }
+      printf ("\n");
+    }
+    usleep (interval); 
+  }
+}
+
+
+int main (int argc, char **argv)
+{
+  struct bb_var *vars[100];
+  int vartypes[100];
+  char *var;
+  int i, nvars;
+
+  bb_init ();
+
+  for (i=1;i<argc;i++) {
+    var = argv[i];
+    vartypes[i-1] = bb_get_type (var);
+    vars[i-1] = bb_get_handle (var);
+    if (vars[i-1] == NULL) {
+      printf("Variable '%s' not found... Exiting.\n", var);
+      exit(-1);
+    }
+  }
+  nvars = i-1;
+
+  mon_varlist (nvars, vartypes, vars);
+
+  exit (0);
+}
